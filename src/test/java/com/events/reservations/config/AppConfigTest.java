11e69792d98dc8112362484038ed6664bb869229
Flávio Marinho <flaviomarinho.tec@gmail.com>
2026-07-21 17:19:59 -0300

    Add route render

--- src/test/java/com/events/reservations/config/AppConfigTest.java
+++ src/test/java/com/events/reservations/config/AppConfigTest.java
@@ -21,10 +21,12 @@ class AppConfigTest {
     @Test
     void shouldAllowLoopbackOriginForFrontendRequests() throws Exception {
         mockMvc.perform(options("/api/events")
-                        .header(HttpHeaders.ORIGIN, "http://127.0.0.1:8080")
+//                        .header(HttpHeaders.ORIGIN, "http://127.0.0.1:8080")
                         .header(HttpHeaders.ORIGIN, "https://projeto-paripassu-frontend-deploy.onrender.com/")
                         .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET"))
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://127.0.0.1:8080"));
+//                .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://127.0.0.1:8080"));
+                .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "https://projeto-paripassu-frontend-deploy.onrender.com/"));
+
     }
 }
